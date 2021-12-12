a1cbc3ff00f4509ca288b5311d05e523d8598e28
WillianChavez <williamchavezamaya@gmail.com>
2021-12-11 19:26:09 -0600

    debuging code js

--- scripts.js
+++ scripts.js
@@ -4,23 +4,32 @@ import { dias } from './dias.js'
 const resultadosDIV = document.querySelectorAll('.resultado')
 const grupoBotones = document.querySelectorAll('.botones')
 const botonesMostrar = document.querySelectorAll('.disable')
+
 let botonesEmpezar = []
+
 grupoBotones.forEach((botones) => botonesEmpezar.push(botones.firstElementChild))
 
 const obtenerNumeroAleatorio = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
 const crearListItem = (items) => {
-    const lista = document.createElement('ul')
+    const lista = document.createElement('UL')
+    const fragmento = document.createDocumentFragment()
     items.forEach((item) => {
         const li = document.createElement('LI')
         li.classList.add('list-item')
         li.textContent = item
-        lista.appendChild(li)
+        fragmento.appendChild(li)
     })
 
+    lista.appendChild(fragmento)
     return lista
 }
 
+const asignarTiempoDeVisibilidad = (timeoutActivo, milisegundos) => {
+    timeoutActivos[timeoutActivo] = setTimeout(() => {
+        resultadosDIV[timeoutActivo].textContent = ''
+    }, milisegundos)
+}
 let timeoutActivos = []
 // ronda 1
 botonesEmpezar[0].addEventListener('click', () => {
@@ -28,9 +37,7 @@ botonesEmpezar[0].addEventListener('click', () => {
     const numeroAleatorio = obtenerNumeroAleatorio(1, 31)
     resultadosDIV[0].textContent = numeroAleatorio
 
-    timeoutActivos[0] = setTimeout(() => {
-        resultadosDIV[0].textContent = ''
-    }, 5000)
+    asignarTiempoDeVisibilidad(0, 5000)
 })
 
 // ronda 2
@@ -44,18 +51,12 @@ botonesEmpezar[1].addEventListener('click', () => {
         numeroAleatorio1 = obtenerNumeroAleatorio(1, 31)
         numeroAleatorio2 = obtenerNumeroAleatorio(1, 31)
         numeroAleatorio3 = obtenerNumeroAleatorio(1, 31)
-    } while (
-        numeroAleatorio1 == numeroAleatorio2 ||
-        numeroAleatorio1 == numeroAleatorio3 ||
-        numeroAleatorio2 == numeroAleatorio3
-    )
+    } while (numeroAleatorio1 == numeroAleatorio2 || numeroAleatorio1 == numeroAleatorio3 || numeroAleatorio2 == numeroAleatorio3)
 
     numerosRonda2.push(numeroAleatorio1, numeroAleatorio2, numeroAleatorio3)
     resultadosDIV[1].textContent = `${numeroAleatorio1}, ${numeroAleatorio2}, ${numeroAleatorio3}`
 
-    timeoutActivos[1] = setTimeout(() => {
-        resultadosDIV[1].textContent = ''
-    }, 5000)
+    asignarTiempoDeVisibilidad(1, 5000)
 })
 
 botonesMostrar[0].addEventListener('click', () => {
@@ -63,9 +64,7 @@ botonesMostrar[0].addEventListener('click', () => {
     resultadosDIV[1].textContent = ''
     resultadosDIV[1].textContent = numerosRonda2.join(', ')
 
-    timeoutActivos[1] = setTimeout(() => {
-        resultadosDIV[1].textContent = ''
-    }, 5000)
+    asignarTiempoDeVisibilidad(1, 5000)
 })
 
 // ronda 3
@@ -85,20 +84,17 @@ botonesEmpezar[2].addEventListener('click', () => {
     listaDeTitulos = crearListItem([titulo1, titulo2, titulo3])
 
     resultadosDIV[2].textContent = ''
-
     resultadosDIV[2].appendChild(listaDeTitulos)
-    timeoutActivos[2] = setTimeout(() => {
-        resultadosDIV[2].textContent = ''
-    }, 5000)
+
+    asignarTiempoDeVisibilidad(2, 5000)
 })
 
 botonesMostrar[1].addEventListener('click', () => {
     clearTimeout(timeoutActivos[2])
     resultadosDIV[2].textContent = ''
     resultadosDIV[2].appendChild(listaDeTitulos)
-    timeoutActivos[2] = setTimeout(() => {
-        resultadosDIV[2].textContent = ''
-    }, 5000)
+
+    asignarTiempoDeVisibilidad(2, 5000)
 })
 // ronda 4
 let diaRonda4
@@ -106,21 +102,17 @@ botonesEmpezar[3].addEventListener('click', () => {
     clearTimeout(timeoutActivos[3])
     botonesMostrar[2].classList.remove('disable')
     diaRonda4 = ''
-
     diaRonda4 = dias[obtenerNumeroAleatorio(0, 6)]
     resultadosDIV[3].textContent = diaRonda4
 
-    timeoutActivos[3] = setTimeout(() => {
-        resultadosDIV[3].textContent = ''
-    }, 5000)
+    asignarTiempoDeVisibilidad(3, 5000)
 })
 
 botonesMostrar[2].addEventListener('click', () => {
     clearTimeout(timeoutActivos[3])
     resultadosDIV[3].textContent = diaRonda4
-    timeoutActivos[3] = setTimeout(() => {
-        resultadosDIV[3].textContent = ''
-    }, 5000)
+
+    asignarTiempoDeVisibilidad(3, 5000)
 })
 
 // ronda 5
@@ -131,14 +123,11 @@ botonesEmpezar[4].addEventListener('click', () => {
     numeroRonda5 = obtenerNumeroAleatorio(1, 5)
     resultadosDIV[4].textContent = numeroRonda5
 
-    timeoutActivos[4] = setTimeout(() => {
-        resultadosDIV[4].textContent = ''
-    }, 5000)
+    asignarTiempoDeVisibilidad(4, 5000)
 })
 botonesMostrar[3].addEventListener('click', () => {
     clearTimeout(timeoutActivos[4])
     resultadosDIV[4].textContent = numeroRonda5
-    timeoutActivos[4] = setTimeout(() => {
-        resultadosDIV[4].textContent = ''
-    }, 5000)
+
+    asignarTiempoDeVisibilidad(4, 5000)
 })
